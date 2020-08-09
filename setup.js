8a29e2563958ad4179f52421a3c0e127fa73170d
Patrick Hein <mmf.ipat@gmail.com>
2020-08-09 03:45:32 +0200

    Version 1.0.0

---
+++ setup.js
@@ -0,0 +1,130 @@
+const tmi = require('tmi.js');
+
+let setupButton = document.querySelector("#pointsetupbutton");
+let setupScreen = document.querySelector("#channelpointsetup");
+let setupCancelButton = document.querySelector("#setupcancel");
+let channelInput = document.querySelector("#channel");
+let urlRequestButton = document.querySelector("#url-request");
+
+
+let rewardId;
+let actualChannelName;
+function scanChannelReward(channel) {
+    var twitchClient = new tmi.client({
+        options: {
+            debug: false
+        },
+        connection: {
+            cluster: "aws",
+            reconnect: true
+        },
+        channels: [channel]
+    });
+
+    twitchClient.connect();
+    twitchClient.on('chat', (channel, userstate, message) => {
+        if (userstate['custom-reward-id'] && userstate['badges'].broadcaster && message == "!setup") {
+            clearInterval(observerIntervalId);
+            channelPlaceholder.style = "color: #44EE44";
+            channelPlaceholder.innerHTML = "<b>Success!</b> TTS reward recognized. :)";
+            rewardId = userstate['custom-reward-id'];
+            setupButton.style = "background-color:rgba(20, 50, 20, 0.6);";
+            actualChannelName = channelInput.value;
+            document.getElementById('current-channel').innerHTML = "Logged in as " + actualChannelName;
+            checkReadynessForButton();
+            setTimeout(() => {
+                setupScreen.style = "display: none;";
+                channelPlaceholder.style = "color: #AAAAAA";
+                observerIntervalId = setInterval(observerCallback, 600);
+            }, 6000);
+            twitchClient.disconnect();
+        }
+    });
+}
+
+let channelPlaceholder = document.querySelector("#channel-placeholder");
+let dotCounter = 1;
+let observerCallback = () => {
+    let progressName = "Observing chat of " + channelInput.value;
+
+    for (let i = 0; i < dotCounter; i++) {
+        progressName += ".";
+    }
+    if (dotCounter++ >= 3) dotCounter = 1;
+
+    channelPlaceholder.innerHTML = progressName;
+};
+
+let observerIntervalId = setInterval(observerCallback, 600);
+
+let cacheTtsRewardId;
+setupButton.onclick = () => {
+    if (!channelInput.value) return;
+
+    setupScreen.style = "";
+
+    cacheTtsRewardId = rewardId;
+    scanChannelReward(channelInput.value);
+    checkReadynessForButton();
+}
+
+setupCancelButton.onclick = () => {
+    setupScreen.style = "display: none;";
+
+    rewardId = cacheTtsRewardId;
+    checkReadynessForButton();
+}
+
+channelInput.onchange = () => {
+    if (!channelInput.value || channelInput.value == "") {
+        setupButton.style = "display: none;"
+    } else {
+        setupButton.style = ""
+    }
+    setupButton.style = "background-color:rgba(0, 0, 0, 0.6);";
+    checkReadynessForButton();
+}
+
+function checkReadynessForButton() {
+    if (channelInput.value && channelInput.value != "" && rewardId) {
+        document.getElementById("url-request").style = "";
+    } else {
+        document.getElementById("url-request").style = "display: none;";
+    }
+}
+
+let urlDisplay = document.getElementById("url-display");
+let urlInput = document.getElementById("url-input");
+urlRequestButton.onclick = () => {
+    let voice = document.getElementById("voice-selection").value;
+    let url = "https://tts.ipat.live?&c=" + encodeURIComponent(actualChannelName)
+    + "&r=" + encodeURIComponent(rewardId)
+    + "&t=" + encodeURIComponent(voice ? voice : "Brian");
+
+    urlInput.value = url;
+    urlDisplay.style = "";
+}
+
+
+/*Dropdown Menu*/
+$('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    document.getElementById("voice-placeholder").style = "";
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
+/*End Dropdown Menu*/
+
+
+$('.dropdown-menu li').click(() => {
+    let input = '<b>' + document.getElementById("voice-selection").value + '</b>';
+    document.querySelector('#volume-placeholder').innerHTML = input;
+});
---
+++ tts.js
@@ -0,0 +1,137 @@
+const fetch = require('node-fetch');
+const tmi = require('tmi.js');
+
+const urlParams = new URLSearchParams(window.location.href);
+var settings = {
+    streamer: urlParams.get('c'),
+    reward: urlParams.get('r'),
+    tts_voice: urlParams.get('t')
+};
+
+if (!settings.streamer || !settings.reward || !settings.tts_voice) {
+    console.log(settings);
+
+    location.replace("https://tts.ipat.live/setup");
+}
+
+let msg_queue = [];
+let audio_queue = [];
+let audio = document.querySelector("audio");
+audio.onended = () => {
+    if (audio_queue.length == 0) return;
+    audio_queue = audio_queue.slice(1);
+    if (audio_queue.length >= 1) {
+        audio.src = audio_queue[0];
+        audio.play();
+    } else {
+        // clearInterval(audio_queue_interval_id);
+        audio_queue_interval();
+        // audio_queue_interval_id = setInterval(audio_queue_interval, 6000);
+    }
+}
+
+let currentPromise;
+function audio_queue_interval() {
+    if (msg_queue.length > 0) {
+        let msg = msg_queue.shift();
+
+        let promise = fetch("https://cors-anywhere.herokuapp.com/https://streamlabs.com/polly/speak", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                voice: msg[0].charAt(0).toUpperCase() + msg[0].slice(1),
+                text: msg[1]
+            })
+        }).then(r => r.json());
+
+        if (!currentPromise) {
+            currentPromise = promise.then(json => {
+                let speak_url = json.speak_url;
+
+                audio_queue.push(speak_url);
+                if (audio_queue.length <= 1) {
+                    audio.src = speak_url;
+                    audio.play();
+                }
+            });
+        } else {
+            currentPromise.then(() => {
+                promise.then(json => {
+                    let speak_url = json.speak_url;
+    
+                    audio_queue.push(speak_url);
+                    if (audio_queue.length <= 1) {
+                        audio.src = speak_url;
+                        audio.play();
+                    }
+                });
+            });
+            currentPromise = promise;
+        }
+    }
+};
+// let audio_queue_interval_id = setInterval(audio_queue_interval, 6000);
+
+var twitchClient = new tmi.client({
+    options: {
+        debug: false
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    channels: [settings.streamer]
+});
+
+let voices = ["nicole", "kevin", "enrique", "tatyana", "russell", "lotte", "geraint", "carmen", "mads", "penelope", "mia", "joanna", "matthew", "brian", "seoyeon", "ruben", "ricardo", "maxim", "lea", "giorgio", "carla", "naja", "maja", "astrid", "ivy", "kimberly", "chantal", "amy", "vicki", "marlene", "ewa", "conchita", "camila", "karl", "zeina", "miguel", "mathieu", "justin", "lucia", "jacek", "bianca", "takumi", "ines", "gwyneth", "cristiano", "mizuki", "celine", "zhiyu", "jan", "liv", "joey", "raveena", "filiz", "dora", "salli", "aditi", "vitoria", "emma", "lupe", "hans", "kendra"]
+twitchClient.connect();
+twitchClient.on('chat', (channel, userstate, message) => {
+    if (userstate['custom-reward-id'] && userstate['custom-reward-id'] == settings.reward) {
+        if (message == "") return;
+
+        let voice = settings.tts_voice.toLowerCase();
+
+        while (true) {
+            let nextVoiceIndex = calcBatchSize(message);
+            let text = message.slice(0, nextVoiceIndex).trim();
+            if (text && text != "") {
+                msg_queue.push([voice, text]);
+                if (msg_queue.length <= 1) {
+                    // clearInterval(audio_queue_interval_id);
+                    audio_queue_interval();
+                    // audio_queue_interval_id = setInterval(audio_queue_interval, 6000);
+                }
+            }
+
+            if (typeof nextVoiceIndex == 'undefined') return;
+
+            message = message.slice(nextVoiceIndex);
+            let colonIndex = message.indexOf(":");
+            voice = message.slice(0, colonIndex).toLowerCase();
+            message = message.slice(colonIndex + 1);
+        }
+    }
+});
+
+function calcBatchSize(message) {
+    let voiceMap = {}
+    for (let voiceIt of voices) {
+        let newVoiceIndex = message.toLowerCase().indexOf(voiceIt + ":");
+        if (newVoiceIndex >= 0) {
+            voiceMap[voiceIt] = newVoiceIndex;
+        }
+    }
+    let lowestVoiceKey;
+    for (let voiceKey of Object.keys(voiceMap)) {
+        if (!lowestVoiceKey || voiceMap[voiceKey] < voiceMap[lowestVoiceKey]) lowestVoiceKey = voiceKey;
+    }
+    if (lowestVoiceKey) return voiceMap[lowestVoiceKey];
+}
+
+let skipButton = document.querySelector('#skip');
+skipButton.onclick = event => {
+    audio.pause();
+    audio.onended();
+}
